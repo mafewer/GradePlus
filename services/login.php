d45143965c08e65f25373563376e92e7c80e0fb2
Moaaz M Elshabasy <mmmybelshaba@mun.ca>
2024-09-22 00:35:36 -0230

    change in login.php

--- services/login.php
+++ services/login.php
@@ -15,51 +15,49 @@ if (isset($_SESSION['logtime']) && isset($_SESSION['username'])) {
         unset($_SESSION['dname']);
     }
 }
-
-$username = htmlspecialchars($_POST['username'] ?? '');
-$email = htmlspecialchars($_POST['email'] ?? '');
-$password = htmlspecialchars($_POST['password'] ?? '');
-
-if ((empty($username) || empty($email)) && empty($password)) {
-    $empty = 1;
-} else {
-
-    $conn = mysqli_connect('localhost', 'gradeplusclient', 'gradeplussql', 'gradeplus');
-
-    if ($conn->connect_error) {
-        $error = 1;
-    } 
-    
-    $sqlCommand = $conn->prepare("SELECT username, dname FROM login WHERE (username = ? OR email = ?) AND password = ?");
-    $sqlCommand->bind_param("sss", $username, $email, $password);
-    
-    if ($sqlCommand->execute()) {
-        $sqlCommand->store_result();
-        if ($sqlCommand->num_rows > 0) {
-            $sqlCommand->bind_result($username, $dname);
-            $sqlCommand->fetch();
-    
-            $_SESSION['logtime'] = time() + (60 * 6);
-            $_SESSION['username'] = $username;
-            $_SESSION['dname'] = $dname;
-            $loggedin = 1;
-            $sqlUpdate = $conn->prepare("UPDATE login SET loggedin = ? WHERE username = ?");
-            $sqlUpdate->bind_param("is", $loggedin, $username);
-            if ($username == "admin" || $email == "admin@gradeplus.com") {
-                header('Location: admin.php');
-            } else {
-                header('Location: account.php');
-            }
-            $success = 1;
-        } else {
-            $incorrect = 1;
-        }
+if (isset($_POST['username'])) {
+    $username = htmlspecialchars($_POST['username'] ?? '');
+    $email = htmlspecialchars($_POST['email'] ?? '');
+    $password = htmlspecialchars($_POST['password'] ?? '');
+    if ((empty($username) || empty($email)) && empty($password)) {
+        $empty = 1;
     } else {
-        $error = 1;
+        try {
+            $conn = mysqli_connect('localhost', 'gradeplusclient', 'gradeplussql', 'gradeplus');
+        } catch (exception $e){
+            $error = 1;
+        } 
+        
+        $sqlCommand = $conn->prepare("SELECT username, dname FROM login WHERE (username = ? OR email = ?) AND password = ?");
+        $sqlCommand->bind_param("sss", $username, $email, $password);
+        
+        if ($sqlCommand->execute()) {
+            $sqlCommand->store_result();
+            if ($sqlCommand->num_rows > 0) {
+                $sqlCommand->bind_result($username, $dname);
+                $sqlCommand->fetch();
+        
+                $_SESSION['logtime'] = time() + (60 * 6);
+                $_SESSION['username'] = $username;
+                $_SESSION['dname'] = $dname;
+                $loggedin = 1;
+                $sqlUpdate = $conn->prepare("UPDATE login SET loggedin = ? WHERE username = ?");
+                $sqlUpdate->bind_param("is", $loggedin, $username);
+                if ($username == "admin" || $email == "admin@gradeplus.com") {
+                    header('Location: admin.php');
+                } else {
+                    header('Location: account.php');
+                }
+                $success = 1;
+            } else {
+                $incorrect = 1;
+            }
+        } else {
+            $error = 1;
+        }
+        
+        $sqlCommand->close();
+        $conn->close();
     }
-    
-    $sqlCommand->close();
-    $conn->close();
 }
-
 ?>
