23239dc8567b2c43ea003147732150279cc3f33b
Jordan <jfbrown@mun.ca>
2024-09-23 01:36:51 -0230

    Reformatted landing page and added register button.

--- index.php
+++ index.php
@@ -10,56 +10,16 @@
 
 <body class="white-text">
     <div class="container">
-        <h2>
+        <h1>
             Welcome to GradePlus!
-        </h2>
+        </h1>
         <div class="flow-text">
             View your grades and peer reviews at one glance!
         </div>
         <br>
-        <div class="flow-text">
-        </div>
         <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">build</i>
-                <h5>Localhost Setup</h5>
-            </div>
-            <p>
-                <?php echo "This line is generated from the back-end. If you are reading this, it means that you have correctly set up
-            your localhost. Good Job! Try playing with the Dark Mode!"; ?>
-            </p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">code</i>
-                <h5>Chosen Languages</h5>
-            </div>
-            <p>Front-End - HTML, CSS, Materialize, JavaScript, jQuery</p>
-            <p>Back-End - PHP, MySQL</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">group</i>
-                <h5>Team Members</h5>
-            </div>
-            <p>Akash, Abdulrahman, Moaaz, Matthew, Jordan, Shane, Ben, Daniel</p>
-            <p>Could you please provide me with your full name, student number, and github username. The repository will
-                be locked to private soon. <a target="_blank" href="https://forms.gle/v8JnuRDR4bSDf6JZA">Google Sign Up
-                    Sheet</a></p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">sentiment_satisfied</i>
-                <h5>Happy Coding</h5>
-            </div>
-            <p>Welcome to the team! I hope you will enjoy this Fall 2024 Semester and I can guide our team to success
-                with our collective hard work!
-            </p>
-        </div>
+        
+        <button class="button">Get Started <i class="material-icons prefix">arrow_forward</i></button>
     </div>
 </body>
 <script src="js/theme.js"></script>
