1f44620c2de4865c6b9817b77b6045690f90962f
Jordan <jfbrown@mun.ca>
2024-09-23 22:49:47 -0230

    Added year calculation

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
+<?php
+$year = date("Y");
+?>
+
+<html>
+<script src="js/copyright.js"></script>
 <footer class="page-footer" style="text-align: center;">
 <div class = "container">
-<p><i class="material-icons">copyright</i> Copyright <span class="footer-date">2024</span> GradePlus</p> 
+<p><i class="material-icons">copyright</i> Copyright <?php echo $year ?> GradePlus</p> 
 </div>   
 </footer>
 <style>
@@ -14,3 +20,4 @@ main {
     flex: 1 0 auto;
     }
 </style>
+</html>
