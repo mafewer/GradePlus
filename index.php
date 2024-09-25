1c4c7b1b33550ed911eee0e4a85d8a1ecdb9e79f
Ben Thomas <brjthomas@mun.ca>
2024-09-25 14:50:42 -0230

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,45 @@
 <?php session_start(); ?>
 <html>
+
+
 <?php include("header.php"); ?>
 
 <head>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <title>GradePlus</title>
+    <title>GradePlus - Home</title>
     <link rel="icon" href="img/logoGreen.png">
 </head>
 
 <body class="white-text">
+    <img src="img/indexback.png" class="indexback">
     <div class="container">
-        <h2>
-            Welcome to GradePlus!
-        </h2>
-        <div class="flow-text">
-            View your grades and peer reviews at one glance!
-        </div>
-        <br>
-        <div class="flow-text">
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">build</i>
-                <h5>Localhost Setup</h5>
+        <div class="holder col s12">
+            <h1 class="welcome">
+                <span class="welcome">Welcome</span>
+            </h1>
+            <div class="flow-text subhead">
+                View your grades and peer reviews at one glance!
             </div>
-            <p>
-                <?php echo "This line is generated from the back-end. If you are reading this, it means that you have correctly set up
-            your localhost. Good Job! Try playing with the Dark Mode!"; ?>
-            </p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">code</i>
-                <h5>Chosen Languages</h5>
-            </div>
-            <p>Front-End - HTML, CSS, Materialize, JavaScript, jQuery</p>
-            <p>Back-End - PHP, MySQL</p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">group</i>
-                <h5>Team Members</h5>
-            </div>
-            <p>Akash, Abdulrahman, Moaaz, Matthew, Jordan, Shane, Ben, Daniel</p>
-            <p>Could you please provide me with your full name, student number, and github username. The repository will
-                be locked to private soon. <a target="_blank" href="https://forms.gle/v8JnuRDR4bSDf6JZA">Google Sign Up
-                    Sheet</a></p>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-            <div class="c1">
-                <i class="material-icons">sentiment_satisfied</i>
-                <h5>Happy Coding</h5>
-            </div>
-            <p>Welcome to the team! I hope you will enjoy this Fall 2024 Semester and I can guide our team to success
-                with our collective hard work!
-            </p>
+            <a href="login.php" class="waves-effect waves-light btn-large getstarted">
+                <div class="icon-holder">Get Started <i class="material-icons getarrow">arrow_forward</i></div>
+            </a>
         </div>
     </div>
+    <?php include("footer.php"); ?>
 </body>
 <script src="js/theme.js"></script>
+<script>
+    const lang = ["Welcome", "Bienvenido", "Bienvenue", "Willkommen", "स्वागत है", "ようこそ", "환영합니다", "خوش آمدید"];
+    var index = 1;
+    setInterval(() => {
+        $("span.welcome").fadeOut(500, function() {
+            $("span.welcome").text(lang[index]).fadeIn(500);
+        });
+        index++;
+        if (index == lang.length) {
+            index = 0;
+        }
+    }, 5000);
+</script>
 
 </html>
