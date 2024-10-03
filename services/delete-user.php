5e69e02891ae961d4b0ff98b9638c44ec1ae2b2e
Shane Hillier <sthillier@mun.ca>
2024-10-03 12:32:19 -0230

    Created delete user microservice

---
+++ services/delete-user.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+// Service to delete user account
+if ($_GET["authorize"] == "gradeplus") {
+    if (!isset($_SESSION['username']) || $_SESSION['username'] == 'admin') {
+        header('Location: ../login.php');
+    }
+    try {
+        $currentName = $_SESSION['username'];
+        $conn = mysqli_connect("localhost", "gradeplusclient", "gradeplussql", "gradeplus");
+        if (!$conn) {
+            error_log("SQL connection failed: " . mysqli_connect_error());
+        }
+
+        // Check if user exists
+        $checkUserSql = sprintf("SELECT 1 FROM login WHERE username = '%s'", $currentName);
+        $result = mysqli_query($conn, $checkUserSql);
+        $row = mysqli_fetch_array($result);
+
+        if (!$result) {
+            error_log("User exists check failed: " . mysqli_error($conn));
+        }
+
+        if ($row[0] != 0) {
+            // Delete user
+            $deleteUserSql = sprintf("DELETE FROM login WHERE username = '%s'", $currentName);
+            $result = mysqli_query($conn, $deleteUserSql);
+            if ($result) {
+                echo("User deleted successfully!");
+                $success = 1;
+                session_unset();
+            } else {
+                error_log("Delete user failed: " . mysqli_error($conn));
+                $error = 1;
+            }
+        } else {
+            echo("User is not found!");
+        }
+    } catch (Exception $e) {
+        // SQL error
+        $success = 0;
+        $error = 1;
+    }
+    mysqli_close($conn);
+    header('Content-Type: application/json');
+    echo json_encode(["success" => $success,"error" => $error]);
+} else {
+    // User is not authorized
+    header("Location: illegal.php");
+}
