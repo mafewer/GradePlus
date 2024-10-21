926c87a8a05ca2fa6f906609e0ea34acb91f740b
Matthew Fewer <mafewer@mun.ca>
2024-10-21 13:44:40 -0230

    Update RegisterUserCest.php

--- tests/Acceptance/RegisterUserCest.php
+++ tests/Acceptance/RegisterUserCest.php
@@ -6,11 +6,6 @@ class RegisterUserCest {
     public function RegisterUser(AcceptanceTester $I) {
         $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-        //Reset the database so I know exactly what is in it
-        $I->sendPost('/services/reset-demo.php', [
-            'authorize' => 'gradeplus'
-        ]);
-
         //Test that I can make a new account that isn't in the system.
         $I->sendPost('/services/register.php', [
             'authorize' => 'gradeplus',
