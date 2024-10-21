1968c203c01253919e695718f4132add26f659cc
Matthew Fewer <mafewer@mun.ca>
2024-10-21 15:37:42 -0230

    Create GetAssignmentsCest.php

---
+++ tests/Acceptance/GetAssignmentsCest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Tests\Support\AcceptanceTester;
+
+class GetAssignmentsCest
+{
+    public function GetAssignments(AcceptanceTester $I)
+    {
+        // Ensure we are hitting the correct endpoint
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $I->sendPost('/services/reset-demo.php', [
+            'authorize' => 'gradeplus'
+        ]);
+        
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $I->sendPost('/services/get-assignments.php', [
+            'authorize' => 'gradeplus',
+            'course_code' => 'ECE 6400'
+        ]);
+
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => 1, 'error' => 0, "illegal"=>0, 'data'=>[
+            [
+                'assignment_name' => 'A1',
+                'assignment_file' => null,
+                'description' => 'I am a description 1',
+                'due_date' => null,
+            ],
+            [
+                'assignment_name' => 'A2',
+                'assignment_file' => null,
+                'description' => 'I am a description 3',
+                'due_date' => null,
+            ]
+        ], 'message' => "Assignments retrieved successfully."]);
+    }
+}
